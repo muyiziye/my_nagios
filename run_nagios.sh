14ee27c4d8ef1ae2fbb41096872a993bada3a882
LiuYang <liu-yang91@qq.com>
2020-04-20 21:43:54 +0800

    delete some useless files

--- run_nagios.sh
+++ run_nagios.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-NGS_PATH="/home/ly/data/code/my_nagios/nagios_opt/"
-GRAPH_PATH="/home/ly/data/code/my_nagios/nagios_graph/"
+NGS_PATH="/home/ly/data/code/my_nagios/nagios/"
+#GRAPH_PATH="/home/ly/data/code/my_nagios/nagios_graph/"
 
 docker run -d --name nagios1 --rm   \
-	-v ${NGS_PATH}etc/:/opt/nagios/etc/   \
+	-v ${NGS_PATH}etc:/opt/nagios/etc/   \
 	-v ${NGS_PATH}var:/opt/nagios/var/   \
-	-v ${GRAPH_PATH}custom_plugin:/opt/Custom-Nagios-Plugins \
-	-v ${GRAPH_PATH}nagiosgraph-rrd:/opt/nagiosgraph/var/rrd \
-       	-p 0.0.0.0:443:443 muyiziye/my_nagios:version-1.0.2
+	-v ${NGS_PATH}plugin:/opt/Custom-Nagios-Plugins \
+	-v ${NGS_PATH}data/rrd:/opt/nagiosgraph/var/rrd \
+       	-p 0.0.0.0:443:443 muyiziye/my_nagios:version-1.0.3
 
 
